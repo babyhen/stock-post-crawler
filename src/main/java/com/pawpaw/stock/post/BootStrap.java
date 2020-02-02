0eb17185e433befafd63e5ef2b3eb0b3a0bc5e2a
Jixin Liu <187157249@qq.com>
2020-02-02 19:21:14 +0800

    ll

--- src/main/java/com/pawpaw/stock/post/BootStrap.java
+++ src/main/java/com/pawpaw/stock/post/BootStrap.java
@@ -22,11 +22,18 @@ public class BootStrap {
 
 
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("useage:");
+            System.out.println("stockcode startDate endDate");
+            System.out.println("for example: 300324 20190101,20190201");
+            return;
+        }
+
         PawpawWebApplication application = new PawpawWebApplication();
         application.run(BootStrap.class);
-        String stockCode = "300324";
-        String startDate = "20190202";
-        String endDate = "20191001";
+        String stockCode = args[0];
+        String startDate = args[1];
+        String endDate = args[2];
         Date begin = TimeUtil.parseDate(startDate, TIME_FORMAT_8);
         Date end = TimeUtil.parseDate(endDate, TIME_FORMAT_8);
 
